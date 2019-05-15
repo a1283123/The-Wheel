4148ce387ea5b56ebc0c901930ca15f0706b5dac
allen_home <a1283123@gmail.com>
2019-05-16 00:24:00 +0800

    node

---
+++ node/src/index.js
@@ -0,0 +1,79 @@
+const mysql=require('mysql');
+const express=require('express');
+const bodyparser=require('body-parser');
+const moment = require('moment-timezone');
+const multer = require('multer');
+const fs = require('fs');
+const cors = require('cors');
+const uuidv4 = require('uuid/v4');
+
+
+var app=express();
+
+// 查看 HTTP HEADER 的 Content-Type: application/x-www-form-urlencoded
+app.use(bodyparser.urlencoded({extended: false}));
+
+// 查看 HTTP HEADER 的 Content-Type: application/json
+app.use(bodyparser.json());
+
+
+var whitelist = ['http://localhost:5555', 'http:// 192.168.1.111:5555', undefined,'http://localhost:3000'];
+var corsOptions = {
+    credentials: true,
+    origin: function (origin, callback) {
+        console.log('origin: '+origin);
+        if (whitelist.indexOf(origin) !== -1) {
+            callback(null, true)
+        } else {
+            callback(new Error('Not allowed by CORS'))
+        }
+    }
+};
+app.use(cors(corsOptions));
+
+
+
+const upload = multer({dest: 'tmp_uploads/'});
+
+
+
+var mysqlConnection=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'the_wheel',
+    multipleStatements:true
+
+});
+
+mysqlConnection.connect((err)=>{
+    if(!err)
+        console.log('DB connection succeded');
+    else
+        console.log('DB connection failed \n Error:'+JSON.stringify(err,undefined,2));
+})
+
+
+//拿到所有會員資料
+app.get('/prouduct',(req,res)=>{
+    mysqlConnection.query('SELECT*FROM prouduct',(err,rows,fields)=>{
+
+       
+        if(!err)       
+        res.send(rows)
+        else
+        console.log(err);
+    })
+});
+
+
+
+
+
+
+
+
+app.listen(5555, ()=>{
+    console.log('server running');
+});
+
